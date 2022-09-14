0d08054ba512a5b6ca63f975d6482d5561ed6d5f
vanec06 <chavezgalindezvanesa@gmail.com>
2022-09-14 16:35:37 -0500

    Iniciando repositorio

---
+++ src/Productos.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import './Productos.css';
+
+const list = [
+    {
+        id:1,
+        descripcion:'PC Samsung',
+            precio:2800000,
+        },
+    {
+        id:2,
+        descripcion:'PC Asus',
+        precio:2300000,
+    },
+    {
+        id:3,
+        descripcion:'Mackbook air ml',
+        precio:4300000,
+    }
+]; 
+function buscar(nombreLibro){
+    return function(item){
+        return item.descripcion.toLowerCase().includes(nombreLibro.toLowerCase())
+    }
+}
+
+class Productos extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            list,
+            nombreLibro: '',
+        };
+        
+        this.Eliminar = this.Eliminar.bind(this);
+        this.onSearchChange= this.onSearchChange.bind(this);
+    }
+
+    Eliminar(id) {
+        const isNotId = item => item.id !== id;
+        const listaActualizada = this.state.list.filter(isNotId);
+        this.setState({ list: listaActualizada});
+    }   
+
+    onSearchChange(event){
+        this.setState({ nombreLibro: event.target.value })
+    }
+    render() {
+        return (
+           <div className="content"> 
+                 <h2>Lista de Productos</h2>
+                <form>
+                    <label>filtrar por nombre del libro </label>
+                    <input type="text" className='Buscador' placeholder='Buscar...' onChange={this.onSearchChange}></input>
+                </form> 
+                <div className='tabla'>                      
+                    <table>
+                        <div>
+                            <th>Id  Producto</th>
+                            <th>Descripción  Producto</th>
+                            <th>Precio</th>
+                            <th>Acciones</th>
+                                    {
+                                        this.state.list.filter(buscar(this.state.nombreLibro)).map (item=>
+                                             <tbody key={item.id}>
+                                            <tr>
+                                                <td>
+                                                    {
+                                                        item.id
+                                                    }
+                                                </td>
+                                                <td>
+                                                    {
+                                                        item.descripcion
+                                                    }
+                                                </td>
+                                                <td>
+                                                    {
+                                                        item.precio
+                                                    }
+                                                </td>
+                                                <td>
+                                                    <button
+                                                        onClick={()=> this.Eliminar(item.id)}
+                                                        type="button"
+                                                    >
+                                                    Eliminar   
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </tbody>  
+                                     )}
+                        </div>
+                    </table>
+                </div>
+            </div>
+        );
+    };
+};
+
+export default Productos;
